ca4bbd1086e33489ec8590f85ddb9bc22c1d7289
sofieb18 <98337235+sofieb18@users.noreply.github.com>
2022-04-08 17:43:05 +0100

    elements added

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
 function displayTemperature(response) {
+  console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
-  console.log(response.data);
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
 }
 
 let apiKey = "fa6ac0eec48a37d445f557c6867cf755";
