64bd079d804f15206d2423af2078a0bf7bd7c5c6
sofieb18 <98337235+sofieb18@users.noreply.github.com>
2022-04-08 17:21:45 +0100

    update

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+}
+
 let apiKey = "fa6ac0eec48a37d445f557c6867cf755";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
+
+console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);
