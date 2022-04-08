aa00d9b5a7fce96459c39078dc684cceeeea0b62
sofieb18 <98337235+sofieb18@users.noreply.github.com>
2022-04-08 17:32:37 +0100

    added data

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  console.log(response.data);
 }
 
 let apiKey = "fa6ac0eec48a37d445f557c6867cf755";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Swindon&appid=${apiKey}&units=metric`;
 
 console.log(apiUrl);
 axios.get(apiUrl).then(displayTemperature);
