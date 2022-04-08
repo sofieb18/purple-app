46ff7e1a1d4799c2e4adebcc889d9b43be6bc9e3
sofieb18 <98337235+sofieb18@users.noreply.github.com>
2022-04-08 21:51:21 +0100

    date update

--- src/app.js
+++ src/app.js
@@ -2,8 +2,20 @@ function formatDate(timestamp) {
   let date = new Date(timestamp);
   let hours = date.getHours();
   let minutes = date.getMinutes();
-  let day = date.getDay();
-  return `${day} ${hours} ${minutes}`;
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
 }
 
 function displayTemperature(response) {
